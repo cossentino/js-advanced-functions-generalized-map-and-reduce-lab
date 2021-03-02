48de6a8b37d65f0a814ceef1bbbd44ecbd20c424
Ian Cossentino <icossentino@gmail.com>
2021-03-02 14:23:38 -0500

    commit tree e302ae7577a8c303725ea96c255edb0599c30ccb

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+
+
+
+
+function map(array, mapFunc) {
+  const r = []
+  for (let i = 0; i < array.length; i++) {
+    r.push(mapFunc(array[i]))
+  }
+  return r
+}
+
+
+
+function reduce (array, reducerFunc, startingPoint=true) {
+  let accumulator = startingPoint
+  const reducer = (accum, current) => reducerFunc(accum, current)
+  for (let i = 0; i < array.length; i++) {
+    accumulator = reducer(accumulator, array[i])
+  }
+  if (startingPoint === true && typeof accumulator === "number") {
+    return accumulator - 1
+  } else {
+    return accumulator
+  }
+}
